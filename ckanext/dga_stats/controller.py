689bdf8628ff3d2a1af53b7a7de90a70cf33a7a3
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2025-04-18 01:08:55 +0300

    chore: remove old code

--- ckanext/dga_stats/controller.py
+++
@@ -1,9 +0,0 @@
-
-from ckan.lib.base import BaseController
-
-import ckanext.dga_stats.utils as utils
-
-
-class StatsController(BaseController):
-    def index(self):
-        return utils.index()
--- ckanext/dga_stats/plugin.py
+++ ckanext/dga_stats/plugin.py
@@ -1,43 +1,31 @@
+from __future__ import annotations
 
 import ckan.plugins as p
+import ckan.plugins.toolkit as tk
 import datetime as datetime
-from logging import getLogger
-from ckanext.dga_stats.views import get_blueprints
+import logging
 
-log = getLogger(__name__)
+log = logging.getLogger(__name__)
 
 
 def date_range():
     return list(reversed(list(range(2013, datetime.datetime.now().year + 1))))
 
 
+@tk.blanket.blueprints
 class StatsPlugin(p.SingletonPlugin):
     """Stats plugin."""
 
     p.implements(p.IConfigurer, inherit=True)
     p.implements(p.ITemplateHelpers)
-    p.implements(p.IBlueprint)
 
     # ITemplateHelpers
-
     def get_helpers(self):
-        """Register the most_popular_groups() function above as a template
-        helper function.
-
-        """
-        # Template helper function names should begin with the name of the
-        # extension they belong to, to avoid clashing with functions from
-        # other extensions.
         return {"date_range": date_range}
 
     # IConfigurer
 
     def update_config(self, config):
-        p.toolkit.add_template_directory(config, "../templates")
-        p.toolkit.add_public_directory(config, "../public")
-        p.toolkit.add_resource("../public/ckanext/stats", "ckanext_dga_stats")
-
-    # IBlueprint
-
-    def get_blueprint(self):
-        return get_blueprints()
+        p.toolkit.add_template_directory(config, "templates")
+        p.toolkit.add_public_directory(config, "public")
+        p.toolkit.add_resource("public/ckanext/stats", "ckanext_dga_stats")
